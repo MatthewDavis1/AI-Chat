375daae32ba29f3fdfc3f30e8e3cf7c8e69c2a29
Matthew Davis <mattdavis047@gmail.com>
2024-09-25 18:53:16 -0400

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Matt Davis on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
